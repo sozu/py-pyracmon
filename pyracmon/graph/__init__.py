aef74f8a6d6801cdc8e0c62b8d4ecd603bd2ec90
sozu <sozuberry@gmail.com>
2019-12-19 15:18:54 +0900

    Fix import error

--- pyracmon/graph/__init__.py
+++ pyracmon/graph/__init__.py
@@ -1,6 +1,6 @@
 from .graph import new_graph, Graph, IdentifyPolicy
 from .spec import GraphSpec
-from .serialize import head
+from .serialize import as_is, head
 
 
 __all__ = ["new_graph", "GraphSpec", "Graph", "IdentifyPolicy", "as_is", "head"]
